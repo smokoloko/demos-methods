da5cfdb334b70b1dfa8c2be1ed032aba1a9f5c1d
TeodorPeev <t.peev1@gmail.com>
2018-07-09 10:36:52 +0300

    Known methods

---
+++ methods-demos/ConsoleApp1/used-methods.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int>() { 1, 2, 4 };
+
+            Console.WriteLine(string.Join("--",list));
+        }
+    }
+}
