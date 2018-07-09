5595376b3d28aa286687c5a96dc5c6ca963da94d
TeodorPeev <t.peev1@gmail.com>
2018-07-09 12:38:36 +0300

    Exception handling

---
+++ methods-demos/ExceptionHandling/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = File.ReadAllLines("../../../test1.txt");
+            
+            foreach(var line in text)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
